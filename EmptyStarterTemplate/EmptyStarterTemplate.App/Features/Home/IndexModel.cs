495622a14e5bae84e38f770c69a0557609b3e317
Jon Blankenship <jon@jonblankenship.com>
2019-01-25 16:55:43 -0600

    Create EmptyStarterTemplate

---
+++ EmptyStarterTemplate/EmptyStarterTemplate.App/Features/Home/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+
+namespace EmptyStarterTemplate.App.Features.Home
+{
+    public class IndexModel: BlazorComponent
+    {
+        public string Title => "Empty Starter Template";
+    }
+}
